334bc7c52028cb4a3c527e2e76a8bcf59f49d6de
shayne <79330+shayne@users.noreply.github.com>
2024-04-15 20:51:26 -0400

    remove hardcoded sha

--- scripts/generate-services-options-json.sh
+++ scripts/generate-services-options-json.sh
@@ -6,7 +6,6 @@ repo="NixOS/nixpkgs"
 branch="release-23.11"
 
 commit_sha=$(curl -s "https://api.github.com/repos/$repo/commits/$branch" | jq -r '.sha')
-commit_sha="684f622ae8353f0c5efcddcfe6b28ab6c1d62277"
 tarball_url="https://api.github.com/repos/$repo/tarball/$commit_sha"
 
 nix eval \
